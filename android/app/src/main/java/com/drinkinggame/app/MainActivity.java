da07872c535c1a9b69c255670c06542636160556
Pekka Tapani Kukonlehto <pekkakukonlehto@outlook.com>
2025-06-14 20:16:36 +0300

    Final, product. Have fun! :)

---
+++ android/app/src/main/java/com/drinkinggame/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.drinkinggame.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.drinkinggame.app',
+  appName: 'drinking-game',
+  webDir: 'dist'
+};
+
+export default config;
